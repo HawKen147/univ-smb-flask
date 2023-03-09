cbe26df0e72bbf16c35cc1b10b4984c547d15190
Pierry BENOIT <bpierry@hotmail.fr>
2023-03-09 12:13:13 +0000

    maj

--- iptables.py
+++ iptables.py
@@ -1,8 +1,11 @@
 # save this as app.py
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template("index.html")
+
+    
