bbe4815d170b9a82b3a5e161a9681d1b91ab98f7
Pierry BENOIT <bpierry@hotmail.fr>
2023-03-09 12:48:33 +0000

    maj

--- iptables.py
+++ iptables.py
@@ -8,4 +8,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-    
+@app.route("/start")
+def index_html():
+    return render_template("index.html")
+
+@app.route("/alias")
+def Alias():
+    return render_template("Alias.html")
+
+@app.route("/rules_filter")
+def rules_filter():
+    return render_template("nat.html")
+
+@app.route("/rules_nat_add")
+def rules_nat_add():
+    return render_template("ajouter_nat.html")
