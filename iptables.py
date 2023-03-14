004586fdf0bc347355ef1e53df06cc85286ddd9d
Pierry BENOIT <bpierry@hotmail.fr>
2023-03-14 13:26:10 +0000

    maj

--- iptables.py
+++ iptables.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -12,6 +13,9 @@ def index_html():
 
 @app.route("/alias")
 def Alias():
+    nb_row = nb_row_in_json('static/alias.json')
+    """for i in len(nb_row):
+        return_table_td_tr()"""
     return render_template("Alias.html")
 
 @app.route("/rules_filter")
@@ -21,3 +25,13 @@ def rules_filter():
 @app.route("/rules_nat_add")
 def rules_nat_add():
     return render_template("ajouter_nat.html")
+
+def nb_row_in_json(file_name):
+    with open(file_name,'r') as file:   
+        data = json.load(file) 
+    print(len(data))
+    return len(data)
+
+
+def return_table_td_tr ():
+    return render_template("alias_table.html")
